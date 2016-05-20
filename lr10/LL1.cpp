590fc55845973b05505b026b21769adc04c74032
Irishka256 <irishka251994@mail.ru>
2016-05-20 19:17:35 +0700

    https://github.com/Irishka256/TYP_5.git

--- lr10/LL1.cpp
+++ lr10/LL1.cpp
@@ -105,421 +105,442 @@ int LL1::LL_1() {
 			}
 		}
 		else{ //в верхушке магазина нетерминал
-			switch(m[z]){ //по нетерминалу в верхушке магазина
-				case NS:
+			switch (m[z]){
+			case NS:
 				m[z++] = TEnd;
 				m[z++] = NProgram;
 				break;
-				case NProgram:
-					/*if (type == TPlus || type == TMinus) *///m[z++] = NOper;
+			case NProgram:
 				if (type == TIdent){
 					m[z++] = NProgram;
-					//m[z++] = TTochkaZap;
+					m[z++] = TTochkaZap;
 					m[z++] = NOpisPerem;
-				}if (type == TTypedef){
+				}
+				else if (type == TTypedef){
 					m[z++] = NProgram;
 					m[z++] = TTochkaZap;
 					m[z++] = NMassiv;
-				}else if (type == TVoid){
+				}
+				else if (type == TVoid){
 					m[z++] = NProgram;
 					m[z++] = NMain;
-				} else if (type == TInt){
+				}
+				else if (type == TInt){
 					m[z++] = NProgram;
-					//m[z++] = TTochkaZap;
-					m[z++] = NOpisPerem;
-				}else if (type == TDouble){
-					m[z++] = NProgram;
-					//m[z++] = TTochkaZap;
+					m[z++] = TTochkaZap;
 					m[z++] = NOpisPerem;
 				}
-				/*else {
-						sc->PrintError("Неверный символ",l);
-						std::cout<<"Ожидалось "<<m[z]<<std::endl;
-						return -1; 
-				}*/
-				break;
-				case NOpisPerem:
-				if (type == TIdent || type == TInt || type == TDouble){
+				else if (type == TDouble){
+					m[z++] = NProgram;
 					m[z++] = TTochkaZap;
+					m[z++] = NOpisPerem;
+				}
+				break;
+			case NOpisPerem:
+				if (type == TIdent || type == TInt || type == TDouble){
 					m[z++] = NSpisPerem;
 					m[z++] = NType;
 				}
-				else {
-						sc->PrintError("Неверный символ",l);
-						Osh(m[z]);
-						return -1; 
-					}
 				break;
-				case NSpisPerem:
-				if (type == TIdent){ 
+			case NSpisPerem:
+				if (type == TIdent){
 					m[z++] = NElemSpis;
 					m[z++] = TIdent;
 				}
-				else {
-						sc->PrintError("Неверный символ",l);
-						Osh(m[z]);
-						return -1; 
-					}
 				break;
-				case NElemSpis: 
+			case NElemSpis:
 				if (type == TPris){
-					m[z++] = NH1;
+					m[z++] = NSpisPerem2;
+					m[z++] = NVyr;
 					m[z++] = TPris;
-				}else if (type == TZap){
-					m[z++] = NH2;
+				}
+				else if (type == TTochkaZap){
+					epsilon();
+				}
+				else if (type == TLKvScob){
+					m[z++] = NSpisPerem2;
+					m[z++] = NIndexInt;
+					m[z++] = TRKvScob;
+					m[z++] = TConstInt10;
+					m[z++] = TLKvScob;
+				}
+				else if (type == TZap){
+					m[z++] = NSpisPerem;
+					m[z++] = TZap;
 				}
 				break;
-				case NH1:
-				if (type == TLFigScob){
-					m[z++] = NT1;
-					//m[z++] = TTochkaZap;/////////////////////////////////////////
+			case NSpisPerem2:
+				if (type == TTochkaZap){
+					epsilon();
+				}
+				else if (type == TZap){
+					m[z++] = NSpisPerem;
+					m[z++] = TZap;
+				}break;
+			case NIndexInt:
+				if (type == TTochkaZap){
+					epsilon();
+				}
+				else if (type == TLKvScob){
+					m[z++] = NIndexInt;
+					m[z++] = TRKvScob;
+					m[z++] = TConstInt10;
+					m[z++] = TLKvScob;
+				}
+				break;
+			case NIndexVyr:
+				if (type == TPlus || type == TPlusPlus || type == TMinus ||
+					type == TMinusMinus || type == TMul || type == TDiv || type == TMod || type == TMenshe ||
+					type == TMensheRavno || type == TBolshe || type == TBolsheRavno || type == TPris || type == TRavno ||
+					type == TNeRavno || type == TTochkaZap || type == TZap || type == TRSkob){
+					epsilon();
+				}
+				else if (type == TLKvScob){
+					m[z++] = NIndexVyr;
+					m[z++] = TRKvScob;
+					m[z++] = NVyr;
+					m[z++] = TLKvScob;
+				}
+				else if (type == TRKvScob){
+					epsilon();
+				}break;
+			case NOpers:
+				if (type == TIdent || type == TIf || type == TInt || type == TDouble ||
+					type == TTochkaZap || type == TLFigScob){
+					m[z++] = NOpers;
+					m[z++] = NOper1;
+				}
+				else if (type == TRFigScob){
+					epsilon();
+				}break;
+			case NOper1:
+				if (type == TIdent){
+					m[z++] = NPris;
+					m[z++] = TIdent;
+				}
+				else if (type == TIf){
+					m[z++] = NIf;
+				}
+				else if (type == TInt){
+					m[z++] = NSpisPerem;
+					m[z++] = TInt;
+				}
+				else if (type == TDouble){
+					m[z++] = NSpisPerem;
+					m[z++] = TDouble;
+				}
+				else if (type == TTochkaZap){
+					m[z++] = TTochkaZap;
+				}
+				else if (type == TLFigScob){
 					m[z++] = TRFigScob;
-					//m[z++] = TZap;
-					m[z++] = NT;
+					m[z++] = NOpers;
 					m[z++] = TLFigScob;
-				}else if (type == TIdent || type == TConstInt10 || type == TLSkob){ //добавить фигур скобку, обыч скобку
-					m[z++] = NH2;
-					m[z++] = NVyr; //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+				}break;
+			case NPris:
+				if (type == TIdent){
+					m[z++] = NElemSpis;
+					m[z++] = TIdent;
 				}
-				else {
-					sc->PrintError("Неверный символ",l);
-					Osh(m[z]);
-					return -1; 
+				else if (type == TPris){
+					m[z++] = NVyr;
+					m[z++] = TPris;
+				}
+				else if (type == TLKvScob){
+					m[z++] = NVyr;
+					m[z++] = TPris;
+					m[z++] = NIndexVyr;
+				}break;
+			case NVyr:
+				if (type == TIdent){
+					m[z++] = NVyr1;
+				}
+				else if (type == TConstInt10){
+					m[z++] = NVyr1;
+				}
+				else if (type == TPlus){
+					m[z++] = NVyr1;
+					m[z++] = TPlus;
+				}
+				else if (type == TPlusPlus){
+					m[z++] = NVyr1;
+				}
+				else if (type == TMinus){
+					m[z++] = NVyr1;
+					m[z++] = TMinus;
+				}
+				else if (type == TMinusMinus){
+					m[z++] = NVyr1;
+				}
+				else if (type == TLSkob){
+					m[z++] = NVyr1;
+				}break;
+			case NVyr1:
+				if (type == TIdent){
+					m[z++] = NVyr15;
+					m[z++] = NVyr2;
+				}
+				else if (type == TConstInt10){
+					m[z++] = NVyr15;
+					m[z++] = NVyr2;
+				}
+				else if (type == TPlusPlus){
+					m[z++] = NVyr15;
+					m[z++] = NVyr2;
+				}
+				else if (type == TMinusMinus){
+					m[z++] = NVyr15;
+					m[z++] = NVyr2;
+				}
+				else if (type == TLSkob){
+					m[z++] = NVyr15;
+					m[z++] = NVyr2;
 				}
 				break;
-				case NT:
-					if (type == TLFigScob)
-						m[z++] = NH1;
-					else if (/*type == TIdent || */type == TConstInt10/* || type == TLSkob*/){
-						//m[z++] = NT;
-						m[z++] = NT2;
-						m[z++] = TConstInt10;
-					}
-					/*else if (type == TRFigScob){
-						//m[z++] = TRFigScob;
-						//m[z++] = NT;
-						break;
-
-					}*/
-					else {
-						sc->PrintError("Неверный символ",l);
-						Osh(m[z]);
-						return -1; 
-					}
-				break;
-				case NT1: //между скобок
-					if (type == TZap){
-						m[z++] = NH1;
-						m[z++] = TZap;
-					}
-				break;
-				case NT2: //между константами
-					if (type == TZap){
-						m[z++] = NT;
-						m[z++] = TZap;
-					}
-				break;
-				case NH2:
-					if (type == TZap){
-						m[z++] = NSpisPerem;
-						m[z++] = TZap;
-					}
-				break;
-				case NMassiv: 
-					if (type == TTypedef){
-						m[z++] = NIndexInt;
-						m[z++] = TIdent;
-						m[z++] = NType;
-						m[z++] = TTypedef;
+			case NVyr15:
+				if (type == TPlus){
+					m[z++] = NVyr1;
+					m[z++] = TPlus;
+				}
+				else if (type == TMinus){
+					m[z++] = NVyr1;
+					m[z++] = TMinus;
+				}
+				else if (type == TMul || type == TDiv || type == TMod || type == TMenshe ||
+					type == TMensheRavno || type == TBolshe || type == TBolsheRavno || type == TRavno || type == TNeRavno ||
+					type == TTochkaZap || type == TZap || type == TRSkob || type == TRKvScob){
+					epsilon();
+				}break;
+			case NVyr2:
+				if (type == TIdent){
+					m[z++] = NVyr25;
+					m[z++] = NVyr3;
+				}
+				else if (type == TConstInt10){
+					m[z++] = NVyr25;
+					m[z++] = NVyr3;
+				}
+				else if (type == TPlusPlus){
+					m[z++] = NVyr25;
+					m[z++] = NVyr3;
+				}
+				else if (type == TMinusMinus){
+					m[z++] = NVyr25;
+					m[z++] = NVyr3;
+				}
+				else if (type == TLSkob){
+					m[z++] = NVyr25;
+					m[z++] = NVyr3;
 				}
 				break;
-				case NMain: //мб один блок оставить
-					//m[z++] = NMain;
-					m[z++] = NBlock;
+			case NVyr25:
+				if (type == TPlus || type == TMinus){
+					epsilon();
+				}
+				else if (type == TMul){
+					m[z++] = NVyr2;
+					m[z++] = TMul;
+				}
+				else if (type == TDiv){
+					m[z++] = NVyr2;
+					m[z++] = TDiv;
+				}
+				else if (type == TMod){
+					m[z++] = NVyr2;
+					m[z++] = TMod;
+				}
+				else if (type == TMenshe ||type == TMensheRavno || type == TBolshe || type == TBolsheRavno 
+					|| type == TRavno || type == TNeRavno || type == TTochkaZap || type == TZap || 
+					type == TRSkob || type == TRKvScob){
+					epsilon();
+				}
+				break;
+			case NVyr3:
+				if (type == TIdent){
+					m[z++] = NVyr35;
+					m[z++] = NVyr4;
+				}
+				else if (type == TConstInt10){
+					m[z++] = NVyr35;
+					m[z++] = NVyr4;
+				}
+				else if (type == TPlusPlus){
+					m[z++] = NVyr35;
+					m[z++] = NVyr4;
+				}
+				else if (type == TMinusMinus){
+					m[z++] = NVyr35;
+					m[z++] = NVyr4;
+				}
+				else if (type == TLSkob){
+					m[z++] = NVyr35;
+					m[z++] = NVyr4;
+				}break;
+			case NVyr35:
+				if (type == TPlus || type == TMinus || type == TMinusMinus || type == TMul || type == TDiv ||
+					type == TMod){
+					epsilon();
+				}
+				else if (type == TMenshe){
+					m[z++] = NVyr4;
+					m[z++] = TMenshe;
+				}
+				else if (type == TMensheRavno){
+					m[z++] = NVyr4;
+					m[z++] = TMensheRavno;
+				}
+				else if (type == TBolshe){
+					m[z++] = NVyr4;
+					m[z++] = TBolshe;
+				}
+				else if (type == TBolsheRavno){
+					m[z++] = NVyr4;
+					m[z++] = TBolsheRavno;
+				}
+				else if (type == TRavno || type == TNeRavno || type == TTochkaZap || type == TZap || type == TRSkob || type == TRKvScob){
+					epsilon();
+				}break;
+			case NVyr4:
+				if (type == TIdent){
+					m[z++] = NVyr45;
+					m[z++] = NVyr5;
+				}
+				else if (type == TConstInt10){
+					m[z++] = NVyr45;
+					m[z++] = NVyr5;
+				}
+				else if (type == TPlusPlus){
+					m[z++] = NVyr45;
+					m[z++] = NVyr5;
+				}
+				else if (type == TMinusMinus){
+					m[z++] = NVyr45;
+					m[z++] = NVyr5;
+				}
+				else if (type == TLSkob){
+					m[z++] = NVyr45;
+					m[z++] = NVyr5;
+				}break;
+			case NVyr45:
+				if (type == TPlus || type == TMinus || type == TMinusMinus || type == TMul || type == TDiv ||
+					type == TMod || type == TMenshe || type == TMensheRavno || type == TBolshe || type == TBolsheRavno){
+					epsilon();
+				}
+				else if (type == TRavno){
+					m[z++] = NVyr5;
+					m[z++] = TRavno;
+				}
+				else if (type == TNeRavno){
+					m[z++] = NVyr5;
+					m[z++] = TNeRavno;
+				}
+				else if (type == TTochkaZap || type == TZap || type == TRSkob || type == TRKvScob){
+					epsilon();
+				}
+				break;
+			case NVyr5:
+				if (type == TIdent){
+					m[z++] = NVyr55;
+					m[z++] = NIndexVyr;
+					m[z++] = TIdent;
+				}
+				else if (type == TConstInt10){
+					m[z++] = TConstInt10;
+				}
+				else if (type == TPlusPlus){
+					m[z++] = NIndexVyr;
+					m[z++] = TIdent;
+					m[z++] = TPlusPlus;
+				}
+				else if (type == TMinusMinus){
+					m[z++] = NIndexVyr;
+					m[z++] = TIdent;
+					m[z++] = TMinusMinus;
+				}
+				else if (type == TLSkob){
+					m[z++] = TRSkob;
+					m[z++] = NVyr;
+					m[z++] = TLSkob;
+				}break;
+			case NVyr55:
+				if (type == TPlus){
+					epsilon();
+				}
+				else if (type == TPlusPlus){
+					m[z++] = TPlusPlus;
+				}
+				else if (type == TMinus){
+					epsilon();
+				}
+				else if (type == TMinusMinus){
+					m[z++] = TMinusMinus;
+				}
+				else if (type == TMul || type == TDiv || type == TMod || type == TMenshe || type == TMensheRavno ||
+					type == TBolshe || type == TBolsheRavno){
+					epsilon();
+				}
+				else if (type == TPris){
+					m[z++] = NVyr;
+					m[z++] = TPris;
+				}
+				else if (type == TRavno || type == TNeRavno || type == TTochkaZap || type == TZap || type == TRSkob ||
+					type == TRKvScob){
+					epsilon();
+				}break;
+			case NIf:
+				if (type == TIf){
+					m[z++] = NH6;
+					m[z++] = NOper1;
+					m[z++] = TRSkob;
+					m[z++] = NVyr;
+					m[z++] = TLSkob;
+					m[z++] = TIf;
+				}break;
+			case NH6:
+				if (type == TIf){
+					m[z++] = NH7;
+					m[z++] = TElse;
+				}
+				else if (type == TTochkaZap){
+					epsilon();
+				}break;
+			case NH7:
+				if (type == TIf){
+					m[z++] = NIf;
+				}
+				else
+				{
+					m[z++] = NOper1;
+				}break;
+			case NMain:
+				if (type == TVoid){
+					m[z++] = TRFigScob;
+					m[z++] = NOpers;
+					m[z++] = TLFigScob;
 					m[z++] = TRSkob;
 					m[z++] = TLSkob;
 					m[z++] = TMain;
 					m[z++] = TVoid;
-					//m[z++] = TLFigScob; 
-				break; 
-				case NBlock:
-					/*if (type == TLFigScob)
-					{*/
-						m[z++] = TRFigScob;
-						m[z++] = NOper;
-						m[z++] = TLFigScob; //мб не надо???????????????????????
-					//}
-					// вывод ошибки????
-				break;
-				case NOper:
-
-					if (type == TTypedef)
-					{
-						m[z++] = NOper;
-						m[z++] = NMassiv;
-					}
-					else if(type == TInt || type == TDouble)
-					{
-						m[z++] = NOper;
-						m[z++] = NOpisPerem;
-					}
-					else if(type == TIf || type == TTochkaZap || type == TLFigScob){ 
-						m[z++] = NOper;
-						m[z++] = NOper1;
-					}
-					else if (type == TIdent)
-					{
-						m[z++] = NOper;
-						m[z++] = NH8;
-						m[z++] = TIdent;
-					}
-				break;
-				case NH8:
-				if (type == TIdent){
-					//m[z++] = NOpisPerem;
-					m[z++] = NSpisPerem;
-				}
-				else if (type == TLKvScob || type == TPris){
-					m[z++] = NH3;
-					//m[z++] = NPris;
-					//m[z++] = TIdent;
-					//m[z++] = TPris;
-				}
-				else {
-					sc->PrintError("Неверный символ",l);
-					Osh(m[z]);
-					return -1; 
-				}
-				// вывод ошибки????????????????7
-				break;
-				case NOper1:
-				if (type == TIdent){
-					m[z++] = NPris;
-				}else if (type == TIf){
-					m[z++] = NIf;
-				}else if (type == TTochkaZap){
-					m[z++] = TTochkaZap;
-				}else if (type == TLFigScob){
-					m[z++] = NBlock;
 				}break;
-				case NPris:
-					m[z++] = NH3;
+			case NType:
+				if (type == TIdent){
 					m[z++] = TIdent;
-				break;
-				case NH3:
-					if (type == TPris){
-						m[z++] = TTochkaZap;
-						m[z++] = NVyr;
-						m[z++] = TPris;
-					}
-					else
-					{
-						m[z++] = TTochkaZap;
-						m[z++] = NVyr;
-						m[z++] = TPris;
-						m[z++] = NH5;
-					}
-				break;
-				case NH5:
-					if (type == TLKvScob){
-						m[z++] = NH5;
-						m[z++] = TRKvScob;
-						m[z++] = NVyr;
-						m[z++] = TLKvScob;
-					}
-				break;
-				case NElemMass:
-					m[z++] = NH4;
-					m[z++] = TRKvScob;
-					m[z++] = NVyr;
-					m[z++] = TLKvScob;
-					m[z++] = TIdent;
-				break;
-				case NH4:
-					if (type == TLKvScob){
-						m[z++] = NH4;
-						m[z++] = TRKvScob;
-						m[z++] = NVyr;
-						m[z++] = TLKvScob;
-					}
-				break;
-				case NVyr:
-					if (type == TPlus)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TPlus;
-					}
-					else if (type == TMinus)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TMinus;
-					}
-					else 
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-					}
-				break;
-				case NA:
-					if (type == TRavno)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TRavno;
-					}
-					else if (type == TNeRavno)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TNeRavno;
-					}
-					else if (type == TMenshe)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TMenshe;
-					}
-					else if (type == TMensheRavno)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TMensheRavno;
-					}
-					else if (type == TBolshe)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TBolshe;
-					}
-					else if (type == TBolsheRavno)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TBolsheRavno;
-					}
-					else if (type == TPlusPlus)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TPlusPlus;
-					}
-					else if (type == TMinusMinus)
-					{
-						m[z++] = NA;
-						m[z++] = NVyr1;
-						m[z++] = TMinusMinus;
-					}
-				break;
-				case NVyr1:
-					m[z++] = NA1;
-					m[z++] = NVyr2;
-				break;
-				case NA1:
-					if (type == TPlus)
-					{
-						m[z++] = NA1;
-						m[z++] = NVyr2;
-						m[z++] = TPlus;
-					}
-					else if (type == TMinus)
-					{
-						m[z++] = NA1;
-						m[z++] = NVyr2;
-						m[z++] = TMinus;
-					}
-				break;
-				case NVyr2:
-					m[z++] = NA2;
-					m[z++] = NVyr3;
-				break;
-				case NA2:
-					if (type == TMul)
-					{
-						m[z++] = NA2;
-						m[z++] = NVyr3;
-						m[z++] = TMul;
-					}
-					else if (type == TDiv)
-					{
-						m[z++] = NA2;
-						m[z++] = NVyr3;
-						m[z++] = TDiv;
-					}
-					else if (type == TMod)
-					{
-						m[z++] = NA2;
-						m[z++] = NVyr3;
-						m[z++] = TMod;
-					}
-				break;
-				case NVyr3:
-					if (type == TIdent)
-					{
-						m[z++] = NH4;
-						m[z++] = TIdent;
-					}
-					else if (type == TConstInt10)
-					{
-						m[z++] = TConstInt10;
-					}
-					else if (type == TLSkob)
-					{
-						m[z++] = TRSkob;
-						m[z++] = NVyr;
-						m[z++] = TLSkob;
-					}
-					else {
-						sc->PrintError("Неверный символ",l);
-						Osh(m[z]);
-						return -1; 
-					}
-				break;
-				case NIf:
-						m[z++] = NH6;
-						m[z++] = NOper1;
-						m[z++] = TRSkob;
-						m[z++] = NVyr;
-						m[z++] = TLSkob; 
-						m[z++] = TIf;
-				break;
-				case NH6:
-					 if (type == TElse){
-						m[z++] = NH7;
-						m[z++] = TElse; 
-					}
-				break;
-				case NH7:
-					if (type == TIf){
-						m[z++] = NIf;
-					}else 
-					{
-						m[z++] = NOper1;
-					}
-					
-				break;
-				case NType: 
-					if (type == TIdent){
-					m[z++] = TIdent;
-					}else if (type == TInt){
+				}
+				else if (type == TInt){
 					m[z++] = TInt;
-					}else if (type == TDouble){
+				}
+				else if (type == TDouble){
 					m[z++] = TDouble;
 				}break;
+			case NMassiv:
+				if (type == TTypedef){
+					m[z++] = NIndexInt;
+					m[z++] = TIdent;
+					m[z++] = NType;
+					m[z++] = TTypedef;
+				}break;
+			default:
 				break;
-				case NIndexInt: 
-					if (type == TTochkaZap){
-						epsilon();
-					}else if (type == TLKvScob){
-						m[z++] = NIndexInt;
-						m[z++] = TRKvScob;
-						m[z++] = TConstInt10;
-						m[z++] = TLKvScob;
-				}
-				break;
-				default:
-					break;
 			}
 			z--;
 		}
